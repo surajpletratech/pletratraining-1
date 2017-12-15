0ae8a153f58961f366ff71fa575cb11cec9e0d0b
surajpletratech <34180713+surajpletratech@users.noreply.github.com>
2017-12-15 20:41:46 +0530

    Add files via upload

---
+++ src/pages/addcourse/addcourse.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'addcourse.html'
+})
+export class AddCourse{
+   
+    constructor(public navCtrl: NavController, public navParams: NavParams)
+      	{
+                               
+    	}
+}
